4f1ccc340524bf2ed09307fa19107f213cf729ac
Robin Kuiper <robingjkuiper@gmail.com>
2017-10-23 00:15:38 +0200

    Added firebase config

---
+++ src/Lib/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase'
+
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDr2iBwOXrZ4Ec1sykHI47Zmx3tUdi7LpU",
+    authDomain: "dndassist-2870c.firebaseapp.com",
+    databaseURL: "https://dndassist-2870c.firebaseio.com",
+    projectId: "dndassist-2870c",
+    storageBucket: "dndassist-2870c.appspot.com",
+    messagingSenderId: "987962337354"
+  };
+
+firebase.initializeApp(config)
+
+export const GoogleProvider = new firebase.auth.GoogleAuthProvider();
+export const Auth = firebase.auth()
+export default firebase
