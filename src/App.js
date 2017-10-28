ee3782ebc6fe6ffb657e056439aeed4ff7c8e002
Robin Kuiper <robingjkuiper@gmail.com>
2017-10-28 14:42:51 +0200

    Changed load timeout

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ import About from './Containers/About'
 import './App.css';
 
 const __LIMIT__ = process.env.NODE_ENV === "development" ? 10 : 1000
-const __LOAD_TIMEOUT__ = process.env.NODE_ENV === "development" ? 5000 : 1000
+const __LOAD_TIMEOUT__ = process.env.NODE_ENV === "development" ? 700 : 300
 const __LOAD_SHIT__ = process.env.NODE_ENV === "development" ? true : true
 
 // TODO: Maybe get all the items (monsters, spells, etc) from the database here, and pass them through
